e32d6958ae564035647ccdb154012867d6d7dec1
jg-grant95 <jerrin.grant@gmail.com>
2017-06-26 01:31:21 -0400

    attempting to create a number pool

---
+++ app/src/main/java/com/example/jerringiselle/lemonadestand/Question.java
@@ -0,0 +1,20 @@
+package com.example.jerringiselle.lemonadestand;
+import math;
+/**
+ * Created by Jerrin Giselle on 6/23/2017.
+ */
+
+public class Question {
+    public final int MIN = 1;
+    public final int MAX = 99;
+    public int term1[] = new int[10];
+    public int term2[] = new int[10];
+    public int holder = 0;
+
+    for (int i=1; i<=10; i++){
+        term1[holder] = i;
+        holder++;
+    }
+
+
+}
